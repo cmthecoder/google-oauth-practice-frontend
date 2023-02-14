9e7212eb17af93f1f0c15eb7965f87a93b545f47
Chris Mems <christopher.mems@.com>
2023-02-14 16:08:51 -0600

    create sign up

--- src/pages/Signup/Singup.jsx
+++ src/pages/Signup/Singup.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 import styles from './styles.module.css'
 
-function Login() {
+function Signup() {
   const googleAuth = () => {
     window.open(
       `${process.env.REACT_APP_API_URL}/auth/google.callback`,
@@ -12,23 +12,24 @@ function Login() {
 
   return (
     <div className={styles.container}>
-      <h1 className={styles.heading}>Log in Form</h1>
+      <h1 className={styles.heading}>Sign up Form</h1>
       <div className={styles.form_container}>
         <div className={styles.left}>
-          <img src="./images/login.jpg" alt="login" />
+          <img src="./images/signup.jpg" alt="signup" />
         </div>
         <div className={styles.right}>
-          <h2 className={styles.from_heading}>Members Log in</h2>
+          <h2 className={styles.from_heading}>Create Account</h2>
+          <input type="text" className={styles.input} placeholder='Username' />
           <input type="text" className={styles.input} placeholder='Email' />
-          <input type="text" className={styles.input} placeholder='Password' />
-          <button className={styles.btn}>Log In</button>
+          <input type="Password" className={styles.input} placeholder='Password' />
+          <button className={styles.btn}>Sign up</button>
           <p className={styles.text}>or</p>
           <button className={styles.google_btn} onClick={googleAuth}>
             <img src="./images/google.png" alt="google icon" />
-            <span>Sign in with Google</span>
+            <span>Sign up with Google</span>
           </button>
           <p className={styles.text}>
-            New Here ? <Link to='/signup'>Sign up</Link>
+            Already Have Account ? <Link to='/login'>Log in</Link>
           </p>
         </div>
       </div>
@@ -36,4 +37,4 @@ function Login() {
   )
 }
 
-export default Login
+export default Signup
